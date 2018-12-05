65a70435ab23741516408f5b2c5d5562e4c70d09
tlschenkjr <32621285+tlschenkjr@users.noreply.github.com>
2018-12-05 14:14:54 -0600

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDPPm5XY0g69t063XDF4ZDjvmTDIBmH0s2xiWKnD0V67MlK281pTyMLJBR/Cpc92vvvH6sc2t6pC1e6uU2UCHgMrEtQUqzL99Gd6R0mHpiEvj7z4Q5/hXmiNpQZLiM4MZUqh1beNV0b+VeSmsfrcRQu8EZo3+TfrX3vnLB01XMbeeLD6Pud3dzjeu2rNGNtY7xeRdT8KGIezRpmGoREENP3lk5dZmA2vqwQVYZdLMYwG7Ne4PrvF00w9RPhF2xFO7DEVu58nVu3jtAYAprkH0ftPOyjFI9Rwxwz7SmQUQaT6KFXqB3GfVegXgL6zV9++JBDB4+msYHGFKQl0xE0iHLp',
 	}  
 }
